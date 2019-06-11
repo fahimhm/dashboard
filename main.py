ac08ad9d0987c74911560d2d0cefd9d5e5bb82ff
fahim.hadi <fahimhadimaula@gmail.com>
2019-06-11 16:52:30 +0700

    plot menggunakan dash

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 # pandas for data management
 import pandas as pd
 
-# os for manipulating paths
+# os for manipulating path
 import os
 from os.path import join
-
-# Bokeh basics
-from bokeh.io import curdoc
-from bokeh.models.widgets import Tabs
 
 # to download dataset
 from scripts.download_dataset import set_dataset
@@ -16,12 +12,15 @@ from scripts.download_dataset import set_dataset
 from scripts.data_prep import actransform
 from scripts.data_prep import prep4chart
 
-# Each plot-tab is drawn by one script
-# 
+# dash & plotly to plot
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
 
 # call dataset
 file_name = ['Performance_Teknisi_Tracker', 'Performance_WO_Tracker', 'Data_Mentah_Maintenance', 'Data_Mentah']
-url_report = "http://maula.fahim:fahimhadimaula28@reportportal/Reports/Pages/Folder.aspx?ItemPath=%2f99.+Aplikasi+lain-lain%2f04.Aplikasi+Manufacturing%2fPortal+Engineering%2fWork+Order&ViewMode=List"
+url_report = "http://maula.fahim:welcometoparadise21@reportportal/Reports/Pages/Folder.aspx?ItemPath=%2f99.+Aplikasi+lain-lain%2f04.Aplikasi+Manufacturing%2fPortal+Engineering%2fWork+Order&ViewMode=List"
 set_dataset(url_report, file_name)
 
 # read data into dataframes
@@ -34,15 +33,38 @@ data_msn = pd.read_csv(join(os.getcwd(), 'datasets', 'Database Mesin PEA 2019.cs
 data_nwo = pd.read_csv(join(os.getcwd(), 'datasets', 'info_non_wo.csv'), sep=';')
 
 # merging dataset & produce new dataset to correction
-mh = actransform(data_tek, data_wo, data_msn, data_shft, data_nwo)
-mergroup, datareal = prep4chart(df=mh, rpmtdata=data_rpmt, rpairdata=data_raw)
+df = actransform(data_tek, data_wo, data_msn, data_shft, data_nwo)
+mh, wo_pmt, wo_rep = prep4chart(df=df, rpmtdata=data_rpmt, rpairdata=data_raw)
 
-# Create each of the tabs
-# tab1 = ...
-# tab2 = ...
+# dataframe: overlap & shift_kosong
+overlap = pd.read_excel(join(os.getcwd(), 'data_output', 'data_overlap.xlsx'), index_col=0)
+shift_kosong = pd.read_excel(join(os.getcwd(), 'data_output', 'data_overlap.xlsx'), index_col=0)
 
-# Put all the tabs into one application
-# tabs = Tabs(tabs = [tab1, tab2])
+# start to plot
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-# Put the tabs in the current document for display
-# curdoc().add_root(tabs)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div(children=[
+    html.H1(children='VALIDASI DATA'),
+    
+    html.Div(children='''
+        Jumlah Data Yang Perlu Dikoreksi
+    '''),
+    
+    dcc.Graph(
+        id = 'validasi-overlap-shift',
+        figure = {
+            'data': [
+                {'x': overlap['workcenter'].unique().tolist(), 'y': overlap.groupby('workcenter')['name'].count().values.tolist(), 'type': 'bar', 'name': 'overlap'},
+                {'x': shift_kosong['workcenter'].unique().tolist(), 'y': shift_kosong.groupby('workcenter')['name'].count().values.tolist(), 'type': 'bar', 'name': 'shift'}
+            ],
+            'layout': {
+                'title': 'Jumlah Data Overlap & WO Tanpa Info Shift'
+            }
+        }
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
