a39e805d0eea5d872c5054f09d7827a3f914b5f0
fahim.hadi <fahimhadimaula@gmail.com>
2019-06-17 14:06:21 +0700

    dash sudah dibuat tetapi belum berhasil

--- main.py
+++ main.py
@@ -12,8 +12,12 @@ from scripts.download_dataset import set_dataset
 from scripts.data_prep import actransform
 from scripts.data_prep import prep4chart
 
-# to produce plot
-from scripts.app import plot
+# dash & plotly for plotting
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly.graph_objs as go
 
 # call dataset
 file_name = ['Performance_Teknisi_Tracker', 'Performance_WO_Tracker', 'Data_Mentah_Maintenance', 'Data_Mentah']
@@ -33,5 +37,90 @@ data_nwo = pd.read_csv(join(os.getcwd(), 'datasets', 'info_non_wo.csv'), sep=';'
 df = actransform(data_tek, data_wo, data_msn, data_shft, data_nwo)
 mh, wo_pmt, wo_rep = prep4chart(df=df, rpmtdata=data_rpmt, rpairdata=data_raw)
 
+# dataframe: overlap & shift_kosong
+overlap = pd.read_excel(join(os.getcwd(), 'data_output', 'data_overlap.xlsx'), index_col=0)
+shift_kosong = pd.read_excel(join(os.getcwd(), 'data_output', 'shift_kosong.xlsx'), index_col=0)
+
 # start plot
-plot()
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+available_year = mh['year'].unique()
+available_woc = mh['workcenter'].unique()
+
+app.layout = html.Div([
+    html.Div([
+        html.H1('VALIDASI DATA'),
+
+        html.Div('''
+            Jumlah Data Yang Perlu Dikoreksi
+        '''),
+
+        dcc.Graph(
+            id = 'validasi-overlap-shift',
+            figure = {
+                'data': [
+                    {'x': overlap['workcenter'].unique().tolist(), 'y': overlap.groupby('workcenter')['name'].count().values.tolist(), 'type': 'bar', 'name': 'overlap'},
+                    {'x': shift_kosong['workcenter'].unique().tolist(), 'y': shift_kosong.groupby('workcenter')['name'].count().values.tolist(), 'type': 'bar', 'name': 'shift'}
+                ],
+                'layout': {
+                    'title': 'Jumlah Data Overlap & WO Tanpa Info Shift'
+                }
+            }
+        )
+    ], className = 'row-one'),
+    
+    html.Div([
+        html.H1('Productivity vs Realisasi WO'),
+
+        html.Div('''
+            %_Productivity vs %_WO-PJOL vs %_WO-PMT
+        '''),
+
+        html.Div([
+            dcc.Dropdown(
+                id = 'year-selected',
+                options = [{'label': i, 'value': i} for i in available_year],
+                value = 2019
+            ),
+            dcc.Dropdown(
+                id = 'workcenter-selected',
+                options = [{'label': i, 'value': i} for i in available_woc],
+                value = 'Utility'
+            )
+        ], style={'width':'48%', 'float':'right', 'display':'inline-block'}),
+
+        dcc.Graph(id='graphic')
+    ])   
+])
+
+@app.callback(
+    Output('graphic', 'figure'),
+    [Input('year-selected', 'value')])
+def update_graph(year_value, woc_value, mh, wo_pmt, wo_rep):
+    data = mh.merge(wo_pmt, on=['workcenter', 'year', 'month'])
+    data = data.merge(wo_rep, on=['workcenter', 'year', 'month'])
+    data = data[(data['workcenter'] == woc_value) & (data['year'] == year_value)].copy()
+
+    return {
+        'data': [go.Bar(
+            x = data['month'],
+            y = data['%_mhreal']
+        )],
+        'layout': go.Layout(
+            xaxis={
+                'title': 'Month',
+                'type': 'linear'
+            },
+            yaxis={
+                'title': 'realization',
+                'type': 'linear'
+            },
+            margin={'l': 40, 'b': 40, 't':10, 'r': 0},
+            hovermode='closest'
+        )
+    }
+
+# if __name__ == 'app':
+app.run_server(debug=False, use_reloader=False)
